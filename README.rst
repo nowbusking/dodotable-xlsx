a6638f44ad2c4bbe29f091bd53d17a601211bd1f
Hong Minhee <hongminhee@spoqa.com>
2017-07-18 21:17:55 +0900

    Minor version bump

--- README.rst
+++ README.rst
@@ -13,7 +13,7 @@ Excel (.xlsx) exporter for dodotable_.
 Changelog
 ---------
 
-Version 0.2.1
+Version 0.3.0
 ~~~~~~~~~~~~~
 
 To be released.
--- dodotable_xlsx.py
+++ dodotable_xlsx.py
@@ -7,7 +7,7 @@ from xlsxwriter import Workbook
 from xlsxwriter.format import Format
 from xlsxwriter.worksheet import Worksheet
 
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 __all__ = 'write_table_to_workbook', 'write_table_to_worksheet'
 
 
